710dc80f6de1b2182d7263af6adca421d2f9ad2f
Cvr421 <chandravijayk42187@gmail.com>
2025-02-05 16:10:08 +0530

    Routs

--- server.js
+++ server.js
@@ -1,5 +1,59 @@
 express = require("express");
+
+const cors = require("cors");
+const { Pool } = require("pg"); // pg library is the official PostgreSQL client for Node.js
+require("dotenv").config();
+
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+// PostgreSQL Connection
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: 5432, 
+});
+
+app.get("/", (req, res) => {
+    res.send("Backend is running!");
+});
+
+app.get("/users", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT * FROM users");
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
